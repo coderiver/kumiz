d85b8da4b5f057138d93b39d2406aa792fd07b33
hraboviyvadim <hraboviyvadim@gmail.com>
2015-05-19 15:50:14 +0300

    fonts, btns

--- js/common.js
+++ js/common.js
@@ -1,19 +1,143 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// svg snap animations
+	(function() {
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+				function SVGHamburger( el, options ) {
+					this.el = el;
+					this.init();
+				}
 
-	console.log($('body').html());
+				SVGHamburger.prototype.init = function() {
+					this.shapeEl = this.el.querySelector( 'span.morph-shape' );
+
+					var s = Snap( this.shapeEl.querySelector( 'svg' ) );
+					this.pathEl1 = s.select( 'path:nth-of-type(1)' );
+					this.pathEl2 = s.select( 'path:nth-of-type(2)' );
+					this.paths = {
+						reset : {
+							path1 : this.pathEl1.attr( 'd' ),
+							path2 : this.pathEl2.attr( 'd' )
+						},
+						open : this.shapeEl.getAttribute( 'data-morph-open' ).split( ';' ),
+						close : this.shapeEl.getAttribute( 'data-morph-close' ).split( ';' )
+					};
+
+					this.isOpen = false;
+
+					this.initEvents();
+				};
+
+				SVGHamburger.prototype.initEvents = function() {
+					this.el.addEventListener( 'mouseover', this.toggleShow.bind(this) );
+					this.el.addEventListener( 'mouseout', this.toggleHide.bind(this) );
+				};
+
+				SVGHamburger.prototype.toggleShow = function() {
+					var self = this,
+						paths = this.isOpen ? this.paths.close : this.paths.open;
+
+						setTimeout( function() { classie.add( self.el, 'is-open' ); }, 200 );
+
+					this.pathEl1.stop().animate( { 'path' : paths[0] }, 300, mina.easeout, function() {
+						self.pathEl1.stop().animate( { 'path' : self.paths.reset.path1 }, 800, mina.elastic );
+					} );
+					this.pathEl2.stop().animate( { 'path' : paths[1] }, 300, mina.easeout, function() {
+						self.pathEl2.stop().animate( { 'path' : self.paths.reset.path2 }, 800, mina.elastic );
+					} );
+
+					this.isOpen = !this.isOpen;
+				};
+
+
+				SVGHamburger.prototype.toggleHide = function() {
+					var self = this,
+						paths = this.isOpen ? this.paths.close : this.paths.open;
+
+						setTimeout( function() { classie.remove( self.el, 'is-open' ); }, 200 );
+
+					this.pathEl1.stop().animate( { 'path' : paths[0] }, 300, mina.easeout, function() {
+						self.pathEl1.stop().animate( { 'path' : self.paths.reset.path1 }, 800, mina.elastic );
+					} );
+					this.pathEl2.stop().animate( { 'path' : paths[1] }, 300, mina.easeout, function() {
+						self.pathEl2.stop().animate( { 'path' : self.paths.reset.path2 }, 800, mina.elastic );
+					} );
+
+					this.isOpen = !this.isOpen;
+				};
+
+				new SVGHamburger( document.getElementById( 'hamburger' ) );
+
+			})();
+
+	// buttons animations
+	
+			(function() {
+
+				function extend( a, b ) {
+					for( var key in b ) { 
+						if( b.hasOwnProperty( key ) ) {
+							a[key] = b[key];
+						}
+					}
+					return a;
+				}
+				
+				function SVGButton( el, options ) {
+					this.el = el;
+					this.options = extend( {}, this.options );
+					extend( this.options, options );
+					this.init();
+				}
+
+				SVGButton.prototype.options = {
+					speed : { reset : 800, active : 150 },
+					easing : { reset : mina.elastic, active : mina.easein }
+				};
+
+				SVGButton.prototype.init = function() {
+					this.shapeEl = this.el.querySelector( 'span.morph-shape' );
+
+					var s = Snap( this.shapeEl.querySelector( 'svg' ) );
+					this.pathEl = s.select( 'path' );
+					this.paths = {
+						reset : this.pathEl.attr( 'd' ),
+						active : this.shapeEl.getAttribute( 'data-morph-active' )
+					};
+
+					this.initEvents();
+				};
+
+				SVGButton.prototype.initEvents = function() {
+					this.el.addEventListener( 'mousedown', this.down.bind(this) );
+					this.el.addEventListener( 'touchstart', this.down.bind(this) );
+
+					this.el.addEventListener( 'mouseup', this.up.bind(this) );
+					this.el.addEventListener( 'touchend', this.up.bind(this) );
+
+					this.el.addEventListener( 'mouseout', this.up.bind(this) );
+				};
+
+				SVGButton.prototype.down = function() {
+					this.pathEl.stop().animate( { 'path' : this.paths.active }, this.options.speed.active, this.options.easing.active );
+				};
+
+				SVGButton.prototype.up = function() {
+					this.pathEl.stop().animate( { 'path' : this.paths.reset }, this.options.speed.reset, this.options.easing.reset );
+				};
+
+				[].slice.call( document.querySelectorAll( 'button.button--effect-1' ) ).forEach( function( el ) {
+					new SVGButton( el );
+				} );
+
+				[].slice.call( document.querySelectorAll( 'button.button--effect-2' ) ).forEach( function( el ) {
+					new SVGButton( el, {
+						speed : { reset : 650, active : 650 },
+						easing : { reset : mina.elastic, active : mina.elastic }
+					} );
+				} );
+
+			})();			
+		
+
 });
