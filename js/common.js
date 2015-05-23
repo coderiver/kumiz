9b8d33a218be924c5815d62c36ce794f77dfe615
hraboviyvadim <hraboviyvadim@gmail.com>
2015-05-23 17:29:38 +0300

    consist added

--- js/common.js
+++ js/common.js
@@ -1,6 +1,41 @@
 head.ready(function() {
 
+	// tabs
 	
+	function tab() {
+       $(".js-tab").each(function(){
+        	var tab_link = $(this).find("a");
+        	var tab_item = $(this).find("li");
+        	var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+        	tab_cont.hide();
+        	tab_item.first().addClass("is-active");
+        	$(this).parents(".js-tab-group").find(".js-tab1").show();
+
+        	tab_link.on("click", function() {
+        	   	var index = $(this).attr("href");
+        	   	tab_item.removeClass("is-active");
+        	   	$(this).parent().addClass("is-active");
+        	   	tab_cont.hide();
+        	   	$(this).parents(".js-tab-group").find("."+index).show();
+        	   	return false;
+        	});
+       });
+  	}
+  	tab();	
+
+	// parallax effect for bg-images
+	function parallax(){
+		$('.js-parallax').each(function(){
+			var cloud = $(this),
+				yPos = ($(window).scrollTop() / 4);
+
+				cloud.css('top', yPos);
+
+		});
+	}
+	parallax();
+	
+
 	// human block
 
 	function human(){
@@ -57,6 +92,9 @@ head.ready(function() {
 	$(window).scroll(function(){
 		if ($('.js-scroll-down').length) {
 			scrollBtn();
+		};
+		if ($('.js-parallax').length) {
+			parallax();
 		};
 	});
 
@@ -202,7 +240,7 @@ head.ready(function() {
 
 	(function() {
 			
-		$('.js-sysmem-info').on( 'click', function () {
+		$('.js-popup').on( 'click', function () {
 			var somedialog = $(this).data('dialog'),
 
 			somedialog = $('#' + somedialog);
